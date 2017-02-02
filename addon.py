eb76c9d0727e5248f1d9cbd469b7109d7c5a43ce
Chris Browet <koying@semperpax.com>
2017-02-02 13:48:09 +0100

    proxy

--- addon.py
+++ addon.py
@@ -1,13 +1,22 @@
 import xbmcaddon
 import xbmcgui
 import os
+import urllib
  
 addon       = xbmcaddon.Addon()
 addonname   = addon.getAddonInfo('name')
  
+print "Environment:\n"
+print "============\n"
 for key in os.environ.keys():
   print "%30s = %s \n" % (key,os.environ[key])
+print "\nProxies:\n"
+print "=========\n"
 
+proxies = urllib.getproxies()
+for key in proxies.keys():
+  print "%30s = %s \n" % (key,proxies[key])
+  
 line1 = "Hello World!"
 line2 = "We can write anything we want here"
 line3 = "Using Python"
