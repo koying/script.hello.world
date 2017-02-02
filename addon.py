1b29ef426e521f670cee59fcf4e092719f63155b
Chris Browet <koying@semperpax.com>
2017-02-02 13:04:16 +0100

    Print env

--- addon.py
+++ addon.py
@@ -1,9 +1,13 @@
 import xbmcaddon
 import xbmcgui
+import os
  
 addon       = xbmcaddon.Addon()
 addonname   = addon.getAddonInfo('name')
  
+for key in os.environ.keys():
+  print "%30s = %s \n" % (key,os.environ[key])
+
 line1 = "Hello World!"
 line2 = "We can write anything we want here"
 line3 = "Using Python"
